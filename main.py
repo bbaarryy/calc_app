dbc96845e64b021bab6ec17c970735f4f0c7bbd0
Linux Dimon <tux@nomail>
2025-06-21 12:42:34 +0300

    Rebuild all logic

--- main.py
+++ main.py
@@ -2,49 +2,70 @@ import keyboard
 import time
 
 arr = []
+free = ['1','2','3','4','5','6','7','8','9','0','-','+','*','/','(',')',' ']
+cursor_ind = 0
+is_shift = 0
+
+def print_ans(arr):
+    s = ""
+    for i in range(len(arr)-1,-1,-1):
+        if(arr[i] in free):
+            s = arr[i] + s
+        else:
+            break
+
+    s = s.replace(' ','')
+
+    try:
+        ans = str(eval(s))
+        keyboard.write(ans)
+    except:
+        keyboard.write('?')
+
+    print(s)
 
 while True:
     event = keyboard.read_event()
 
-    if event.event_type == keyboard.KEY_UP and event.name == '=' and arr[-1] != '+':
-        s = ""
-        for i in range(len(arr)):
-            s += arr[i]
+    if(event.name == 'shift' and event.event_type == keyboard.KEY_DOWN):
+        is_shift = 1
+    
+    if(event.name == 'shift' and event.event_type == keyboard.KEY_UP):
+        is_shift = 0
 
-        #print(s)
-        arr.clear()
-        try:
-            ans = str(eval(s))
-            
-            for i in range(len(ans)):
-                arr.append(ans[i])
+    
+    elif(event.event_type == keyboard.KEY_UP):
 
-                if(ans[i] == '-'):
-                    keyboard.send(74)
-                elif(ans[i] == '0'):
-                    keyboard.send(11)
-                else:
-                    keyboard.send(int(ans[i]) + 1)
-        except:
-            keyboard.send('shift + 7')
-        
-        #arr.pop(-1)
-        #print(eval(s))
-        print(s)
-        
-        
-    elif(keyboard.is_pressed('shift + =')):
-        arr.append('+')
-    elif(keyboard.is_pressed('shift + 8')):
-        arr.append('*')
-    elif(event.event_type == keyboard.KEY_DOWN):
-        #print(event.name)
-        if(event.name in ['0','1','2','3','4','5','6','7','8','9','−','/']):
-            if(event.name == '−'):
-                arr.append('-')
+        if(event.name == '−'):
+            arr.insert(cursor_ind, '-')
+            cursor_ind+=1
+        elif(event.name == 'left'):
+            cursor_ind -=1
+        elif(event.name == 'right'):
+            cursor_ind += 1
+        elif(event.name == 'backspace'):
+            try:
+                arr.pop(cursor_ind)
+            except:
+                continue
+            cursor_ind -=1
+            cursor_ind = max(0,cursor_ind)
+        elif event.name != 'shift':
+            if(is_shift):
+                if(event.name == '8'):
+                    arr.insert(cursor_ind, '*')
+                    cursor_ind+=1
+                elif(event.name == '='):
+                    arr.insert(cursor_ind, '+')
+                    cursor_ind+=1
+                #print(arr)
             else:
-                arr.append(event.name)
-        elif(event.name == 'space' or event.name == 'backspace'):
-            arr.clear()
+                arr.insert(cursor_ind, event.name)
+                cursor_ind += 1
+                if(arr[-1] == '='):
+                    arr.pop(-1)
+                    print_ans(arr)
+                    arr.clear()
+                    cursor_ind = 0
         
         
