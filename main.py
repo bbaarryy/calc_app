0a774fb3c96e4a4f484f46f2e7a870248fc6538b
Linux Dimon <tux@nomail>
2025-07-27 23:48:57 +0300

    add inline-debug + disable auto-clear after printing ans

--- main.py
+++ main.py
@@ -1,28 +1,43 @@
 import keyboard
 
-FREE = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '+', '*', '/', '(', ')', ' ']
-SHIFT_KEYS = {'=': '+', '8': '*', '9': '(', '0': ')'}
+FREE = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '+', '*', '/', '(', ')', 'space','−','c',\
+        '#']
+SHIFT_KEYS = {'=': '+', '8': '*', '9': '(', '0': ')','3':'#'}
 arr = []
 is_shift = 0
 cursor_ind = 0
+keep_last = 0
+last_ans = ''
 
-
+#answer print_function
 def print_ans(arr, cursor_ind):
+    if('space' in arr):
+        arr.remove('space')
     s = "".join(arr[:cursor_ind + 1])
-    s = s.replace(' ', '')
+    s = s.replace('−', '-')
+    last_ans = ''
 
     try:
         ans = str(eval(s))
+        last_ans = ans
         keyboard.write(ans)
     except:
-        keyboard.write("wtf")
+        pass
 
-    print(s)
-
+    return(last_ans)
 
 while True:
     event = keyboard.read_event()
 
+    #add inline-debug
+    if(len(arr)>0 and arr[-1] == '#'):
+        if(len(arr)>1 and arr[-2] == 'c'):
+            arr.clear()
+            print('clear!')
+            keyboard.send('backspace')
+            keyboard.send('backspace')
+
+    #programm keep all last 100 clicks on keyboard
     if len(arr) > 100:
         arr.pop(0)
 
@@ -31,11 +46,8 @@ while True:
             is_shift = 1
     elif event.event_type == keyboard.KEY_UP:
         match event.name:
-            case 'delete':
-                try:
-                    arr.pop(cursor_ind)
-                except:
-                    continue
+            case 'delete' if len(arr) > cursor_ind:
+                arr.pop(cursor_ind)
             case 'shift' | 'right shift':
                 is_shift = 0
             case 'right':
@@ -43,15 +55,36 @@ while True:
             case 'left':
                 cursor_ind = max(0, cursor_ind - 1)
             case 'backspace' if arr:
-                arr.pop(cursor_ind - 1)1=11+2=3 4+4=8
+                arr.pop(cursor_ind - 1)
                 cursor_ind = max(0, cursor_ind - 1)
             case name if name in SHIFT_KEYS and is_shift:
+
+                if keep_last:
+                    keep_last = 0
+                    
                 arr.insert(cursor_ind, SHIFT_KEYS[name])
                 cursor_ind += 1
             case '=':
-                print_ans(arr, cursor_ind - 1)
+                last_ans = print_ans(arr, cursor_ind - 1)
+                
+                keep_last=1
                 arr.clear()
-                cursor_ind = 0
+                for i in range(len(last_ans)):
+                    arr.append(last_ans[i])
+                cursor_ind = len(arr)
+                print(arr)
             case name if name in FREE:
-                arr.insert(cursor_ind, name)
-                cursor_ind += 1
+
+                if keep_last:
+                    if name =='space':
+                        arr.clear()
+                        cursor_ind =0
+                        keep_last=0
+                    else:
+                        arr.insert(cursor_ind, name)
+                        keep_last=0
+                        cursor_ind +=1
+                else:
+                    arr.insert(cursor_ind, name)
+                    cursor_ind +=1
+                print(arr)
